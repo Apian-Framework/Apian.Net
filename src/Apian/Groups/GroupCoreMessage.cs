29e2bffb266f41555a99cbaa344b429cf1c5b3a1
jim berry <jim@bigfattail.com>
2022-11-07 15:08:41 -0500

    added pause/resume group messages

--- src/Apian/Groups/GroupCoreMessage.cs
+++ src/Apian/Groups/GroupCoreMessage.cs
@@ -12,6 +12,8 @@ namespace Apian
         // and typically end up as ApianCommand payloads and are part of the serial Apian event stream along withwith ApianCoreMessages
 
         public const string CheckpointRequest = "GmmCkRq";
+        public const string PauseAppCore = "GcmPs";
+        public const string ResumeAppCore = "GcmRs";
 
         public GroupCoreMessage(string t, long ts) : base(ApianCoreMessage.kGroupMgr, t, ts) {}
         public GroupCoreMessage() {}
@@ -22,6 +24,25 @@ namespace Apian
         public CheckpointRequestMsg(long ts) : base(GroupCoreMessage.CheckpointRequest, ts) {}
     }
 
+    public class PauseAppCoreMsg : GroupCoreMessage
+    {
+        public string reason; // descriptive "why": ("quorum Lost", "Bob requested"...)
+        public string instanceId; // a unique id for resume to use.
+        public PauseAppCoreMsg(long ts, string _reason, string _instance) : base(GroupCoreMessage.PauseAppCore, ts)
+        {
+            reason = _reason;
+            instanceId = _instance;
+        }
+    }
+
+    public class ResumeAppCoreMsg : GroupCoreMessage
+    {
+        public string instanceId; // using pause command identifier.
+        public ResumeAppCoreMsg(long ts, string isntance) : base(GroupCoreMessage.PauseAppCore, ts)
+        {
+            instanceId = isntance;
+        }
+    }
 
     public class GroupCoreMessageDeserializer : ApianCoreMessageDeserializer
     {
@@ -32,6 +53,8 @@ namespace Apian
                 new  Dictionary<string, Func<string, ApianCoreMessage>>()
                 {
                     {GroupCoreMessage.CheckpointRequest, (s) => JsonConvert.DeserializeObject<CheckpointRequestMsg>(s) },
+                    {GroupCoreMessage.PauseAppCore, (s) => JsonConvert.DeserializeObject<PauseAppCoreMsg>(s) },
+                    {GroupCoreMessage.ResumeAppCore, (s) => JsonConvert.DeserializeObject<ResumeAppCoreMsg>(s) },
                 } ).ToDictionary(x=>x.Key,x=>x.Value);
         }
     }
